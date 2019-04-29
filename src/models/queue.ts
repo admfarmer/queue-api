f6d78d1441c7b4cacd3319ea62a8d4e33b76defb
Thawatchai Saengduan <thawatchai.sea2@gmail.com>
2019-04-29 21:45:09 +0700

    25620429

--- src/models/queue.ts
+++ src/models/queue.ts
@@ -409,7 +409,7 @@ export class QueueModel {
     return db('q4u_queue_detail as qd')
       .select('qd.service_point_id', 'q.queue_interview', 'qd.date_serv as queue_date', 'qd.last_queue', 'qd.room_id',
         'q.queue_number', 'q.hn', 'q.vn', 'qd.queue_id', 'q.date_serv', 'q.time_serv', 'qd.update_date', 'p.title', 'p.first_name', 'p.last_name',
-        'p.birthdate', 'pr.priority_name', 'pr.prority_color',
+        'p.birthdate', 'pr.priority_name', 'pr.priority_id', 'pr.prority_color',
         'r.room_name', 'r.room_number', 'sp.service_point_name', 'sp.department_id', 'q.is_completed')
       .innerJoin('q4u_queue as q', 'q.queue_id', 'qd.queue_id')
       .innerJoin('q4u_person as p', 'p.hn', 'q.hn')
@@ -428,7 +428,7 @@ export class QueueModel {
     return db('q4u_queue_group_detail as qd')
       .select('qd.service_point_id', 'q.queue_interview', 'qd.date_serv as queue_date', 'qd.last_queue', 'qd.room_id',
         'q.queue_number', 'q.queue_running', 'q.hn', 'q.vn', 'qd.queue_id', 'q.date_serv', 'q.time_serv', 'qd.update_date', 'p.title', 'p.first_name', 'p.last_name',
-        'p.birthdate', 'pr.priority_name', 'pr.prority_color', 'q.is_completed',
+        'p.birthdate', 'pr.priority_name', 'pr.priority_id', 'pr.prority_color', 'q.is_completed',
         'r.room_name', 'r.room_number', 'sp.service_point_name')
       .innerJoin('q4u_queue as q', 'q.queue_id', 'qd.queue_id')
       .innerJoin('q4u_person as p', 'p.hn', 'q.hn')
@@ -450,7 +450,7 @@ export class QueueModel {
     let sql = db('q4u_queue as q')
       .select('q.service_point_id', 'q.queue_interview', 'q.date_serv as queue_date', 'q.room_id',
         'q.queue_number', 'q.hn', 'q.vn', 'q.queue_id', 'q.date_serv', 'q.time_serv', 'p.title', 'p.first_name', 'p.last_name',
-        'p.birthdate', 'pr.priority_name', 'pr.prority_color', 'q.is_completed',
+        'p.birthdate', 'pr.priority_name', 'pr.priority_id', 'pr.prority_color', 'q.is_completed',
         'r.room_name', 'r.room_number', 'sp.service_point_name', db.raw(`ifnull(qd.update_date,CURRENT_DATE) as update_date`))
       .innerJoin('q4u_person as p', 'p.hn', 'q.hn')
       .innerJoin('q4u_priorities as pr', 'pr.priority_id', 'q.priority_id')
@@ -473,7 +473,7 @@ export class QueueModel {
       .select(
         'q.queue_number', 'q.hn', 'q.vn', 'q.queue_id', 'q.room_id', 'r.room_name', 'r.room_number',
         'q.date_serv', 'q.time_serv', 'p.title', 'p.first_name', 'p.last_name',
-        'p.birthdate', 'pr.priority_name', 'pr.prority_color', 'sp.service_point_name')
+        'p.birthdate', 'pr.priority_name', 'pr.priority_id', 'pr.prority_color', 'sp.service_point_name')
       .innerJoin('q4u_person as p', 'p.hn', 'q.hn')
       .innerJoin('q4u_priorities as pr', 'pr.priority_id', 'q.priority_id')
       .innerJoin('q4u_service_points as sp', 'sp.service_point_id', 'q.service_point_id')
@@ -488,7 +488,7 @@ export class QueueModel {
     let sql = db('q4u_queue as q')
       .select('q.service_point_id', 'q.date_serv as queue_date', 'qgd.room_id',
         'q.queue_number', 'q.queue_running', 'q.hn', 'q.vn', 'q.queue_id', 'q.queue_interview', 'q.date_serv', 'q.time_serv', 'q.date_update', 'p.title', 'p.first_name', 'p.last_name',
-        'p.birthdate', 'pr.priority_name', 'pr.prority_color',
+        'p.birthdate', 'pr.priority_name', 'pr.priority_id', 'pr.prority_color',
         'r.room_name', 'r.room_number', 'sp.service_point_name')
       // .innerJoin('q4u_queue as q', 'q.queue_id', 'qd.queue_id')
       .innerJoin('q4u_person as p', 'p.hn', 'q.hn')
@@ -519,7 +519,7 @@ export class QueueModel {
     let sql = db('q4u_queue as q')
       .select('q.service_point_id', 'q.date_serv as queue_date', 'qgd.room_id',
         'q.queue_number', 'q.queue_running', 'q.hn', 'q.vn', 'q.queue_id', 'q.queue_interview', 'q.date_serv', 'q.time_serv', 'q.date_update', 'p.title', 'p.first_name', 'p.last_name',
-        'p.birthdate', 'pr.priority_name', 'pr.prority_color',
+        'p.birthdate', 'pr.priority_name', 'pr.priority_id', 'pr.prority_color',
         'r.room_name', 'r.room_number', 'sp.service_point_name')
       // .innerJoin('q4u_queue as q', 'q.queue_id', 'qd.queue_id')
       .innerJoin('q4u_person as p', 'p.hn', 'q.hn')
@@ -559,7 +559,7 @@ export class QueueModel {
     let sql = db('q4u_queue as q')
       .select('q.service_point_id', 'q.date_serv as queue_date', 'q.room_id',
         'q.queue_number', 'q.hn', 'q.vn', 'q.queue_id', 'q.queue_interview', 'q.date_serv', 'q.time_serv', 'q.date_update', 'p.title', 'p.first_name', 'p.last_name',
-        'p.birthdate', 'pr.priority_name', 'pr.prority_color',
+        'p.birthdate', 'pr.priority_name', 'pr.priority_id', 'pr.prority_color',
         'r.room_name', 'r.room_number', 'sp.service_point_name', 'q.is_interview')
       // .innerJoin('q4u_queue as q', 'q.queue_id', 'qd.queue_id')
       .innerJoin('q4u_person as p', 'p.hn', 'q.hn')
@@ -602,7 +602,7 @@ export class QueueModel {
     return db('q4u_queue as q')
       .select('q.service_point_id', 'q.date_serv as queue_date', 'q.room_id',
         'q.queue_number', 'q.hn', 'q.vn', 'q.queue_id', 'q.queue_interview', 'q.date_serv', 'q.time_serv', 'q.date_update', 'p.title', 'p.first_name', 'p.last_name',
-        'p.birthdate', 'pr.priority_name', 'pr.prority_color',
+        'p.birthdate', 'pr.priority_name', 'pr.priority_id', 'pr.prority_color',
         'r.room_name', 'r.room_id', 'r.room_number', 'sp.service_point_name', 'sp2.service_point_name as pending_to_service_point_name')
       .innerJoin('q4u_person as p', 'p.hn', 'q.hn')
       .innerJoin('q4u_priorities as pr', 'pr.priority_id', 'q.priority_id')
@@ -621,7 +621,7 @@ export class QueueModel {
     return db('q4u_queue as q')
       .select('q.service_point_id', 'q.date_serv as queue_date', 'q.room_id',
         'q.queue_number', 'q.hn', 'q.vn', 'q.queue_id', 'q.queue_interview', 'q.date_serv', 'q.time_serv', 'q.date_update', 'p.title', 'p.first_name', 'p.last_name',
-        'p.birthdate', 'pr.priority_name', 'pr.prority_color',
+        'p.birthdate', 'pr.priority_name', 'pr.priority_id', 'pr.prority_color',
         'r.room_name', 'r.room_id', 'r.room_number', 'sp.service_point_name', 'sp2.service_point_name as pending_to_service_point_name')
       .innerJoin('q4u_person as p', 'p.hn', 'q.hn')
       .innerJoin('q4u_priorities as pr', 'pr.priority_id', 'q.priority_id')
